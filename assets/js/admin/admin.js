38368894cf3c60d1c6b7ac32313102b8d31096bd
mustafauysal <me@uysalmustafa.com>
2021-03-02 13:49:40 +0300

    Dynamically activate the current tab on the settings page.

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -1,10 +1,16 @@
 const { jQuery } = window;
 
 jQuery(document).ready(function ($) {
-	$('#pp-setting-tabs').tabs();
+	let activeIndex = 0;
+	if ($('.nav-tab-active').length) {
+		activeIndex = $('.nav-tab-active').parent().index();
+	}
+
+	$('#pp-setting-tabs').tabs({ active: activeIndex });
 	$('.pp-setting-tab').on('click', function () {
 		$('.pp-setting-tab').removeClass('nav-tab-active');
 		$(this).addClass('nav-tab-active');
+		$('#current_section').val($(this).attr('href'));
 	});
 
 	$('#enable_software_licensing').change(function () {
